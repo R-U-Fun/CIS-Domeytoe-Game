c23780f2eb67d5c820b9246a6f91aaf479ae972a
Aaroophan <arophn@gmail.com>
2023-10-20 01:17:49 +0530

    20231020_0117 - CorrectOrNot

--- src/StartGame.js
+++ src/StartGame.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import React, { useRef } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -15,13 +16,27 @@ import Level from './Level';
 import Leaderboard from './Leaderboard';
 import UserProfile from './UserProfile';
 
-function CorrectOrNot(){
-    return null;
+function CorrectOrNot(props){
+    console.log("CorrectorNot");
+    console.log("CorrectorNot "+props.Correct);
+    console.log("CorrectorNot "+props.Answer);
+    if(props.Answer === props.Correct){
+        return(
+            <i class="bi bi-hand-thumbs-up-fill btn btn-danger m-4 fs-1"></i>
+        );
+    }
+    else{
+        return(
+            <i class="bi bi-hand-thumbs-down-fill btn btn-danger m-4 fs-1"></i>
+        );
+    }
 }
 
 function Game(props){
-    console.log(props.question);
-    console.log(props.solution);
+    const inputRef = useRef();
+    console.log("Game "+props.question);
+    let Solution = parseInt(props.solution);
+    console.log("Game "+Solution);
     return(
         <div className="card text-white" style={{ background: 'rgba(0, 0, 0, 0)', border: 'none',display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <a className="btn btn-danger m-4 fs-2 fw-bold" style={{width:"400px"}}>Game</a>
@@ -29,10 +44,11 @@ function Game(props){
             <div className="card-body" style={{ background: 'rgba(0, 0, 0, 0)', border: 'none' }}>
                 <div className="input-group mb-3">
                     <span className="input-group-text" id="basic-addon1"><i className="bi bi-123"></i></span>
-                    <input type="text" className="form-control" placeholder="Answer" aria-label="Answer" aria-describedby="basic-addon1"/>
+                    <input type="text" className="form-control" placeholder="Answer" aria-label="Answer" aria-describedby="basic-addon1" ref={inputRef}/>
                     &nbsp;&nbsp;&nbsp;
-                    <button type="button" className="btn btn-danger" onClick={() => ReactDOM.render(<CorrectOrNot Correct={props.solution} Answer= {null} />, document.getElementById('Box'))}><i className="bi bi-arrow-return-right"></i></button>
+                    <button type="button" className="btn btn-danger" onClick={() => ReactDOM.render(<CorrectOrNot Correct={Solution} Answer={parseInt(inputRef.current.value)} />, document.getElementById('CoN'))}><i className="bi bi-arrow-return-right fw-bold"></i></button>
                 </div>
+                <div id="CoN"></div>
             </div>
         </div>
     );
@@ -42,8 +58,8 @@ export default function StartGame(){
     fetch('https://marcconrad.com/uob/tomato/api.php')
         .then(response => response.json())
         .then(data => {
-            console.log(data.question);
-            console.log(data.solution);
+            console.log("StartGame "+data.question);
+            console.log("StartGame "+data.solution);
             ReactDOM.render(<Game question={data.question} solution= {data.solution} />, document.getElementById('Box'));
         })
         .catch(error => console.error('Error:', error));
