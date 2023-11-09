ec914823abd0934140f4bc471b5276ab7ea8de67
Aaroophan <arophn@gmail.com>
2023-11-09 13:03:21 +0530

    20231109_1302 - DB Returns Wholse

--- src/ExpressTest.js
+++ src/ExpressTest.js
@@ -12,22 +12,21 @@ mongoose.connect('mongodb+srv://Aaroophan:AaroophanMongoDB@cluster0.9y1xdpc.mong
 })
 .catch(err => console.log(err));
 
-// Create a model from the schema
 let User = mongoose.model('User', new mongoose.Schema({
-    UserID: Number,
-    Name: String,
-    Password: String,
-    DailyStreaks: Number,
-    Rank: Number,
-    BestTime: Number,
-    GamesPlayed: Number,
-    GamesWon: Number
+    //User_Aaroophan4:{
+        UserID: Number,
+        Name: String,
+        Password: String,
+        DailyStreaks: Number,
+        Rank: Number,
+        BestTime: Number,
+        GamesPlayed: Number,
+        GamesWon: Number
+    //}
 }));
 
-//let User2 = mongoose.model('User', new mongoose.Schema({ Name: String }));
-
 app.get('/Server/UserProfile', (req, res) => {
-    User.find({ "User_Aaroophan4.Name": "Aaroophan4"})
+    User.find({ "User_Aaroophan4.Name": "Aaroophan4" })
     .then(user => {
         if (user) {
             console.log(user);
@@ -35,13 +34,13 @@ app.get('/Server/UserProfile', (req, res) => {
             res.json(user);
         } else {
             console.log("######################################################### USER NOT FOUND");
+            res.json(user);
         }
     })
     .catch(err => {
         console.log(err);
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
